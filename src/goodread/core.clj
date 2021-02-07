ecf1dd5d859b89e562067b183f98969cdbb205e8
Rafael Herrera <santoElias65537@gmail.com>
2021-02-07 00:31:31 -0700

    added some functions and some organizing

--- src/goodread/core.clj
+++ src/goodread/core.clj
@@ -2,7 +2,6 @@
   (:gen-class)
   (:require [cheshire.core :refer :all]))
 
-
 (def data-base
   {:nameHere {:allBooks ["harry potter" "twilight" "hunger games" "hunger games 2" "hunger games 3"]
               :horror ["hunger games" "hunger games 2" "hunger games 3"]
@@ -13,8 +12,7 @@
 
 (def empty-data-base {})
 
-(def available-functions ["create-user" "create-catagory" "add-book-to-catagory" "delete-book" "delete-user" "get-user" "get-user-catagory"])
-
+(def available-functions ["create-user" "create-catagory" "add-book-to-catagory" "delete-book-everywhere" "delete-book-in-catagory" "delete-user" "get-user" "get-user-catagory"])
 
 (defn create-user
   [name]
@@ -23,7 +21,6 @@
 (defn create-catagory
   [data user catagory]
   (update-in data [(keyword user)] #(conj % (hash-map (keyword catagory) []))))
-
 
 (defn add-book-to-catagory
   [data user catagory book]
@@ -42,23 +39,21 @@
   (assoc-in data [(keyword user) (keyword catagory)]
     (into [] (remove #{book} (get-in data [(keyword user) (keyword catagory)])))))
 
+(defn get-user
+  [data user]
+  ((keyword user) data))
+
+(defn get-user-catagory
+  [data user catagory]
+  ((keyword catagory) ((keyword user) data)))
 
 (defn call
   [this]
   (resolve (symbol this)))
 
-
-
-
 (defn print-list
   [data user catagory]
   (run! println (get-in data [(keyword user)  (keyword catagory)])))
-
-
-(defn get-input
-  []
-  (read-line))
-
 
 (defn does-user-exist?
   "if user exist returns user, if not keep asking for a valid user"
@@ -90,53 +85,69 @@
   [arr]
   (run! println arr))
 
-;make main with cond
+(defn what-to-print
+  [input]
+  (if (does-function-exists available-functions input)
+    (println "proceeds to call function")
+    (do
+      (println "what would you like to do? - make sure of no spelling errors")
+      (print-array available-functions)
+      (println "please enter a valid function."))))
 
 (defn main
   [data]
   (loop [data-base data
          input ""]
-    (print-array available-functions)
-    (println "what would you like to do? - make sure of no spelling errors")
-    (when (does-function-exists available-functions input) (return-JSON (get-user-catagory data-base "nameHere" "horror")))
+    (what-to-print input)
     (if (= input "e")
      (println (generate-string data {:pretty true}))
      (recur data (get-input)))))
-
-
-(defn mainV3
-  [data]
-  (loop [data-base data
-         input ""]
-    (print-array available-functions)
-    (println "what would you like to do? - make sure of no spelling errors")
-    (when (does-function-exists available-functions input) (return-JSON (get-user-catagory data-base "nameHere" "horror")))
-    (cond
-      (= input "e") (println (generate-string data {:pretty true}))
-      (= input "get-user") (return-JSON (get-user data-base (read-line)))
-      :else (recur data (get-input)))))
-
-
 
 (defn -main
    "I don't do a whole lot ... yet."
    [& args]
    (main))
 
-
-
-
-
 (defn return-JSON
   [data]
   (generate-string data))
 
 
-(defn get-user
-  [data user]
-  ((keyword user) data))
 
 
-(defn get-user-catagory
-  [data user catagory]
-  ((keyword catagory) ((keyword user) data)))
+
+;need delete-book-everywhere
+;given book
+;get path to location
+;ex. "book 1" secondUser
+;returns idk some like [[:secondUser :allBooks] [:secondUser :horror]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ; (defn mainV3
+  ;   [data]
+  ;   (loop [data-base data
+  ;          input ""]
+  ;     (print-array available-functions)
+  ;     (println "what would you like to do? - make sure of no spelling errors")
+  ;     (when (does-function-exists available-functions input) (return-JSON (get-user-catagory data-base "nameHere" "horror")))
+  ;     (cond
+  ;       (= input "e") (println (generate-string data {:pretty true}))
+  ;       (= input "get-user") (return-JSON (get-user data-base (read-line)))
+  ;       :else (recur data (get-input)))))
